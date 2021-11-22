3421233a551f08a2b48d428cb45a7868844e83b5
HenryTufano <60906254+HenryTufano@users.noreply.github.com>
2021-11-22 20:19:58 -0300

    First commit

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php
+	$servidor = "127.0.0.1";
+	$usuario = "root";
+	$senha = "henrique";
+	$dbname = "faculdade";
+	
+	//Criar a conexão
+	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+?>
